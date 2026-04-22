939daa2008d5dc34f40b878cc74c138fe4608b30
Ya <gx09950113@gmail.com>
2026-04-22 22:30:38 +0800

    Update input.js

--- scr/input.js
+++ scr/input.js
@@ -1,50 +1,49 @@
 /**
- * 鍵盤輸入監聽器
+ * 鍵盤與滑鼠輸入監聽器
  */
-
 export class InputHandler {
     constructor() {
-        // 記錄當前按下的按鍵狀態
         this.keys = {};
+        this.isMouseLeftPressed = false;
 
-        // 監聽按下按鍵
+        // 鍵盤監聽
         window.addEventListener('keydown', (e) => {
-            // 使用 e.code 可以避開中文輸入法導致的抓不到鍵位問題
             this.keys[e.code] = true;
         });
-
-        // 監聽放開按鍵
         window.addEventListener('keyup', (e) => {
             this.keys[e.code] = false;
         });
+
+        // 滑鼠點擊監聽 (用於攻擊)
+        window.addEventListener('mousedown', (e) => {
+            if (e.button === 0) this.isMouseLeftPressed = true;
+        });
+        window.addEventListener('mouseup', (e) => {
+            if (e.button === 0) this.isMouseLeftPressed = false;
+        });
     }
 
-    /**
-     * 檢查特定按鍵是否正被按下
-     * @param {string} keyCode - 例如 'KeyD', 'KeyA', 'Space', 'ArrowUp'
-     */
     isPressed(keyCode) {
         return this.keys[keyCode] === true;
     }
 
-    /**
-     * 輔助方法：檢查是否正在往左走
-     */
     get isLeft() {
         return this.isPressed('KeyA') || this.isPressed('ArrowLeft');
     }
 
-    /**
-     * 輔助方法：檢查是否正在往右走
-     */
     get isRight() {
         return this.isPressed('KeyD') || this.isPressed('ArrowRight');
     }
 
-    /**
-     * 輔助方法：檢查是否按下跳躍/攻擊
-     */
+    // 空白鍵專用於跳躍
     get isJump() {
         return this.isPressed('Space') || this.isPressed('KeyW') || this.isPressed('ArrowUp');
+    }
+
+    // 滑鼠左鍵專用於攻擊
+    get isAttack() {
+        const attack = this.isMouseLeftPressed;
+        if (attack) this.isMouseLeftPressed = false; // 單次點擊觸發，防止按住連發
+        return attack;
     }
 }
