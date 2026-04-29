46c679a4453f082ac8e1acec769739db9ee8f89f
Ya <gx09950113@gmail.com>
2026-04-29 15:40:40 +0800

    Update background.js

--- scr/background.js
+++ scr/background.js
@@ -1,65 +1,52 @@
 /**
- * 無縫捲軸背景類別
+ * 背景類別 - 處理無縫捲動效果
  */
 export class Background {
     /**
      * @param {number} width - 畫布寬度
      * @param {number} height - 畫布高度
-     * @param {HTMLImageElement} image - 背景圖片物件
+     * @param {HTMLImageElement} image - 已經載入完成的圖片物件
      */
     constructor(width, height, image) {
         this.width = width;
         this.height = height;
         this.image = image;
-        
-        // 背景座標
         this.x = 0;
-        this.y = 0;
-        
-        // 圖片原始寬高
-        this.imageWidth = image.width;
-        this.imageHeight = image.height;
-
-        // 捲軸速度係數 (可根據需求微調背景相對於玩家速度的比例)
-        this.speedModifier = 1; 
+        this.x2 = this.width; // 第二張背景圖，實現無縫連接
     }
 
     /**
      * 更新背景位置
-     * @param {number} playerSpeed - 傳入當前角色的移動速度
+     * @param {number} speed - 捲動速度
      */
-    update(playerSpeed) {
-        // 背景向左移動的距離取決於角色速度
-        this.x -= playerSpeed * this.speedModifier;
+    update(speed) {
+        // 背景向左移動
+        this.x -= speed * 0.2; // 讓背景捲動速度慢一點，產生景深感
+        this.x2 -= speed * 0.2;
 
-        // 如果第一張背景完全移出畫面左側，則重置 x 座標
-        // 這裡假設背景圖寬度大於或等於畫布寬度
-        if (this.x <= -this.imageWidth) {
-            this.x = 0;
+        // 當第一張背景完全移出螢幕左側，將其接到第二張後面
+        if (this.x <= -this.width) {
+            this.x = this.x2 + this.width;
+        }
+        // 當第二張背景完全移出螢幕左側
+        if (this.x2 <= -this.width) {
+            this.x2 = this.x + this.width;
         }
     }
 
     /**
-     * 繪製背景 (兩張銜接)
+     * 繪製背景
      * @param {CanvasRenderingContext2D} ctx 
      */
     draw(ctx) {
-        // 第一張圖
-        ctx.drawImage(
-            this.image, 
-            this.x, 
-            this.y, 
-            this.imageWidth, 
-            this.height
-        );
-
-        // 第二張圖，緊接在第一張後面
-        ctx.drawImage(
-            this.image, 
-            this.x + this.imageWidth, 
-            this.y, 
-            this.imageWidth, 
-            this.height
-        );
+        // 只有在圖片物件存在且載入成功時才繪製
+        if (this.image && this.image.complete) {
+            ctx.drawImage(this.image, this.x, 0, this.width, this.height);
+            ctx.drawImage(this.image, this.x2, 0, this.width, this.height);
+        } else {
+            // 如果圖片還沒好，畫一個深灰色背景當墊底
+            ctx.fillStyle = "#1a1a1a";
+            ctx.fillRect(0, 0, this.width, this.height);
+        }
     }
 }
